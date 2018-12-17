26689c11fa748f10d089bb1f8048ce3b6c75bc0f
Chethan K <krishnamurthy.chethan@gmail.com>
2018-12-17 22:31:09 +0530

    Math operators

---
+++ exercise-3/ex3.1.py
@@ -0,0 +1,36 @@
+print("I will now count my chickens:")
+
+# 30/6 = 5
+# 25 + 5.0 = 30.0 
+print("Hens", 25 + 30/6)
+
+# 100 - 75 % 4
+# 100 - 3 = 97
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs")
+
+# 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# 3 + 2 + 1 - 5 + 4 % 2 - 0.25 + 6
+# 3 + 2 + 1 - 5 + 0 - 0.25 + 6
+# 6 - 5 + 0 - 0.25 + 6
+# 1 - 0.25 + 6 = 0.75 + 6 = 6.75
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# 3 + 2 < 5 - 7
+# 5 < -2 => False
+print(3 + 2 < 5 - 7)
+
+
+print("What is 3 + 2? ", 3 + 2)
+print("What is 5 - 7? ", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more?")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
